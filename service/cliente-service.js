07fccd43d500961abf931660b1e4764963dba3dc
Denis <dineresc@gmail.com>
2021-02-16 16:19:33 -0300

    creating the method to add a new line

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,7 +1,23 @@
+const criarNovaLinha = (nome, email) => {
+    const linhaNovoCliente  = document.createElement('tr')
+    const conteudo = `<td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
+            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        </ul>
+    </td>`;
+    
+    linhaNovoCliente.innerHTML = conteudo
+    return linhaNovoCliente
+}
+
 const http = new XMLHttpRequest()
 
 http.open('GET', 'http://localhost:3000/profile')
 http.send()
+
 http.onload = () => {
     const data = http.response
     console.log(data)
